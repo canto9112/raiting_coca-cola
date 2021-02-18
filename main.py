8783acb1498d003d5a2e5e249d7461d1efa4dc4b
canto9112 <cantoweb112@gmail.com>
2021-02-18 14:40:17 +0300

    Написал функцию получения большого колличества постов

--- main.py
+++ main.py
@@ -4,25 +4,41 @@ from dotenv import load_dotenv
 import os
 
 
-load_dotenv()
-
-TOKEN = os.getenv('VK_SERVICE_TOKEN')
-VERION_API = '5.126'
-
-ask = 'Coca-Cola'
-
-def fetch_json(ask, token, version_api):
+def get_first_page_posts(ask, token, version_api, posts_number,page_number):
     base_url = 'https://api.vk.com/method/newsfeed.search'
     params = {
         'q': ask,
-        'count': 200,
+        'count': posts_number,
         'access_token': token,
+        'start_from': page_number,
         'v': version_api}
     response = requests.get(base_url, params=params)
     response.raise_for_status()
-    return response.json()
+    response = response.json()
+    first_page_posts = []
+    for number in range(0, posts_number):
+        response_text = response['response']['items'][number]['text']
+        first_page_posts.append(response_text)
+    return first_page_posts
 
 
-pprint(fetch_json(ask, TOKEN, VERION_API))
+def fetch_posts(page_number, ask, token, version_api, count_post):
+    all_posts = []
+    for page in range(1, page_number):
+        posts = get_first_page_posts(ask, token, version_api, count_post, page)
+        all_posts.extend(posts)
+    return all_posts
 
-print('')
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    service_token = os.getenv('VK_SERVICE_TOKEN')
+    version_api = '5.126'
+    count_post = 200
+    page_number = 6
+
+    ask = 'coca-cola'
+    all_posts = fetch_posts(page_number, ask, service_token, version_api, count_post)
+    print(len(all_posts))
+
